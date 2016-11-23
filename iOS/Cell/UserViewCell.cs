faf73c8bcae2b5b259275cc12dc1390cb25d1c51
韓世謙 <me4000i@gmail.com>
2016-11-23 18:00:55 +0800

    Table View 1

---
+++ iOS/Cell/UserViewCell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Foundation;
+using UIKit;
+
+namespace Han.iOS
+{
+	public partial class UserViewCell : UITableViewCell
+	{
+		public static readonly NSString Key = new NSString("UserViewCell");
+		public static readonly UINib Nib;
+
+		static UserViewCell()
+		{
+			Nib = UINib.FromName("UserViewCell", NSBundle.MainBundle);
+		}
+
+		protected UserViewCell(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+	}
+}
---
+++ iOS/Cell/UserViewCell.designer.cs
@@ -0,0 +1,18 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+
+namespace Han.iOS
+{
+	[Register("UserViewCell")]
+	partial class UserViewCell
+	{
+		void ReleaseDesignerOutlets()
+		{
+		}
+	}
+}
